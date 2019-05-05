8dff40e1ec62e85610af22f524c9807b24c18626
Enyioma Osondu <o.enyioma@gmail.com>
2019-05-05 21:42:16 +0100

    fixed a problem in the staircase file

--- Enyioma Oluwafemi/staircase.py
+++ Enyioma Oluwafemi/staircase.py
@@ -1,10 +1,10 @@
 def staircase(n):
-    for i in range(n):
+    for i in range(1, n+1):
         print(" " * (n-i) + "#" * i)
 
 
 def staircase(n):
-    for i in range(n):
+    for i in range(1, n+1):
         print(("#"*i).rjust(n))
 
 
